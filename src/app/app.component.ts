89afb29dce53c3d7ff017881c8202b00ffc3e259
Chris Drake <christopher.a.drake@gmail.com>
2022-05-25 16:55:13 -0400

    Updated title of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'niivue-basic-ng';
+  title = 'Basic Niivue Angular App';
 }
